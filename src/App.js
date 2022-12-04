9172be63acd42b01a725174f83de901801567ffc
chrisbrights <softtalents1986@outlook.com>
2022-12-05 08:24:52 +0900

    Tailwind css applied to React frontend

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 // import Token from './artifacts/contracts/Token.sol/Token.json';
 import ERCToken from './artifacts/contracts/ERCToken.sol/ERCToken.json';
 
-const greeterAddress = "0xC2FF81eFE00Eafa8627A094Aaf99d55cCC36B183"
+const greeterAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 // const tokenAddress = "0xc2185EDb108D4042C429b0Bb72A346591C4F0b97"
-const ercTokenAddress = "0x34072804E499D943C03f22A2891814e39C40F351"
+const ercTokenAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 
 function App() {
   const [greeting, setGreetingValue] = useState();
@@ -59,33 +59,76 @@ function App() {
     }
   }
 
-  async function setGreeting() {
-    if (!greeting) return
+  async function handleSubmit(event) {
+    event.preventDefault()
+    await setGreeting(event.target.greetingInput.value)
+    setGreetingValue(event.target.greetingInput.value)
+    event.target.greetingInput.value = ""
+  }
+
+  async function setGreeting(value) {
+    if (!value) return
     if (typeof window.ethereum !== 'undefined') {
       await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner()
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-      const transaction = await contract.setGreeting(greeting)
+      const transaction = await contract.setGreeting(value)
       await transaction.wait()
       fetchGreeting()
     }
   }
 
-  return (
-    <div className="App">
-      <header className='App-header'>
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
-        <input value={greeting} onChange={e => setGreetingValue(e.target.value)}
-          placeholder="Set greeting" />
+  // return (
+  //   <div className="App">
+  //     <header className='App-header'>
+  //       <button onClick={fetchGreeting}>Fetch Greeting</button>
+  //       <button onClick={setGreeting}>Set Greeting</button>
+  //       <input value={greeting} onChange={e => setGreetingValue(e.target.value)}
+  //         placeholder="Set greeting" />
 
-        <br />
-        <button onClick={getBalance}>Get Balance</button>
-        <button onClick={sendCoins}>Send Coins</button>
-        <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
-        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
-      </header>
+  //       <br />
+  //       <button onClick={getBalance}>Get Balance</button>
+  //       <button onClick={sendCoins}>Send Coins</button>
+  //       <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+  //       <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
+  //     </header>
+  //   </div>
+  // )
+
+  return (
+    <div className='w-full max-w-lg container'>
+      <div className='shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4'>
+        <div className='text-gray-600 font-bold text-lg mb-2'>
+          Full Stack React Ethereum Dapp
+        </div>
+        <div className='w-full border-4 p-2 mb-4 rounded border-gray-400'>
+          <div className='text-gray-600 font-bold text-md mb-2'>
+            Fetch Greeting Message From Smart Contract
+          </div>
+          <div className='flex'>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={fetchGreeting}>Fetch Greeting</button>
+          </div>
+        </div>
+        <div className='w-full border-4 p-2 mb-4 rounded border-gray-400'>
+          <div className='text-gray-600 font-bold text-md mb-2'>
+            Set Greeting Message on Smart Contract
+          </div>
+          <form className='flex items-center justify-between'
+            onSubmit={e => handleSubmit(e)}>
+            <input className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus outline-none focus:shadow-outline' name='greetingInput' />
+            <button className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Set Greeting</button>
+          </form>
+        </div>
+        <div className='w-full border-4 p-2 mb-4 rounded border-gray-400 bg-gray-100'>
+          <div className='text-gray-600 font-bold text-md mb-2'>
+            Greeting Message
+          </div>
+          <p>
+            {greeting}
+          </p>
+        </div>
+      </div>
     </div>
   )
 
